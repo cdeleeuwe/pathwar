f2722eb2746c4f66731c6b40a4a109d1c331306e
Cassius de Leeuwe <cadeleeuwe@gmail.com>
2019-10-23 10:53:02 +0700

    should build

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `☠️ Pathwar Land ☠️ `,
+    title: `☠️ Pathwar Land ☠️s `,
     description: `Pathwar is an educational platform with a focus on security/cryptography, where you can go through challenges (courses) to learn new things and hone your skills.`,
     baseUrl: `https://www.pathwar.land`
   },
